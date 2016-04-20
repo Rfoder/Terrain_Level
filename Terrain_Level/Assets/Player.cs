f92e15e0e789eeda7e0e69a279278cd01b9ad1ef
Rfoder <bfcat10@yahoo.com>
2016-04-20 18:49:07 -0400

    Added pickups
    can pick up flint

---
+++ Terrain_Level/Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private int count;
+	public GUIText countText;
+	public GUIText winText;
+
+	void Start()
+	{
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag ==  "WhiteSmoke") {
+
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText();
+		}
+	}
+	void SetCountText(){
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 3) {
+			winText.text = "YOU WIN!";
+		}
+	}
+
+}
