f4a3519f350260afc26fa36c92aafa98c3eb08a2
Rfoder <bfcat10@yahoo.com>
2016-04-21 12:15:19 -0400

    working on text

---
+++ Terrain_Level/Assets/textenter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class textenter : MonoBehaviour {
+
+	private int count;
+	// Use this for initialization
+	void Start () {
+		count = 0;
+	}
+	
+	// Update is called once per frame
+
+
+		void OnTriggerEnter(Collider other) {
+			if (other.gameObject.tag ==  "Textonscreen") {
+				
+				other.gameObject.SetActive(false);
+				count = count + 1;
+
+			}
+	}
+}	
