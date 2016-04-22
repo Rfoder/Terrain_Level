c989797c9b009050c5a412c64517c2e6cea5312a
Rfoder <bfcat10@yahoo.com>
2016-04-22 12:38:48 -0400

    changes

---
+++ Assets/Birds.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Birds : MonoBehaviour {
+	public AudioSource source;
+	public AudioClip Clip;
+	private bool isPlayed;
+
+	public void Awake(){
+		source = GetComponent<AudioSource>();
+		isPlayed = false;
+	}
+	// Use this for initialization
+	public void OnTriggerEnter(Collider other) {
+	if (!isPlayed) {
+			source.Play();
+			isPlayed = true;
+		}
+	}
+	
+	// Update is called once per frame
+public void OnTrigge(Collider other) {
+		if (source.isPlaying);{
+			source.Stop ();
+		}
+	}
+	
+	}
+
---
+++ Assets/text.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class text : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	 
+		gameObject.SetActive (false);
+
+	
+	}
+
+	void OnTriggerEnter () {
+		 
+		gameObject.SetActive (true);
+	}
+
+	void OnTriggerExit () {
+		gameObject.SetActive (false);
+		Destroy (gameObject);
+	}
+
+}
